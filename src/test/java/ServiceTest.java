73021e186107bdbf18300abb357fbdceaec5bbc5
roberton <roberto.nerici@bbc.co.uk>
2014-10-25 18:36:54 +0100

    Added test framework

---
+++ src/test/java/ServiceTest.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ServiceTest {
+
+    @Test
+    public void testSomething() {
+        assertTrue(true);
+    }
+}
+
