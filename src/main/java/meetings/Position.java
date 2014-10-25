c732776ca9a8bc728d531e1386123ffeb307fe28
Alex Sneddon <snedda92@mc-s093801.hack.com>
2014-10-25 20:58:23 +0100

    default constructor added to position.java

--- src/main/java/meetings/Position.java
+++ src/main/java/meetings/Position.java
@@ -1,12 +1,18 @@
 package meetings;
 
 public class Position {
-    final public float latitude;
-    final public float longitude;
+    final public double latitude;
+    final public double longitude;
 
-    public Position (float latitude, float longitude)
+    public Position (double latitude, double longitude)
     {
         this.latitude = latitude;
         this.longitude = longitude;
+    }
+
+    public Position ()
+    {
+        this.latitude = 51.5072;
+        this.longitude = 0.1275;
     }
 }
