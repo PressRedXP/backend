47d7a3770ad99f677a73cd892b672e6309338a46
roberton <roberto.nerici@bbc.co.uk>
2014-10-25 16:14:59 +0100

    Fixed bad merge

--- src/main/java/People.java
+++ src/main/java/People.java
@@ -4,16 +4,11 @@ public class People {
 
     public static void main(String[] args) {
         setPort(Integer.parseInt(System.getenv("PORT")));
-<<<<<<< HEAD
-//        get("/people", (req, res) -> "[Test]");
 
         get("/people", (request, response) -> "[Test]");
 
         get("/hello", "application/json", (request, response) -> {
             return new Person("Mr Animals", "animal");
         }, new JsonTransformer());
-=======
-        get("/people", (req, res) -> "[Test - Alex pushing to heroku]");
->>>>>>> 4b17618cf5cd6bcd07d9f92f615f5a5e21690efa
     }
 }
