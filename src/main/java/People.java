f1ca41a185c4038bd44ce066028d81ceb8ca5d65
roberton <roberto.nerici@bbc.co.uk>
2014-10-25 17:19:56 +0100

    Added new contacts endpoint although returned data is wrong

--- src/main/java/People.java
+++ src/main/java/People.java
@@ -1,14 +1,28 @@
-import static spark.Spark.*;
+import static spark.Spark.get;
+import static spark.Spark.setPort;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class People {
 
     public static void main(String[] args) {
         setPort(Integer.parseInt(System.getenv("PORT")));
 
-        get("/people", (request, response) -> "[Test]");
+        get("/hello", (request, response) -> "hello");
 
-        get("/hello", "application/json", (request, response) -> {
-            return new Person("Mr Animals", "animal");
+        // https://justmeet-backend.herokuapp.com/people/mrbuttons/contacts
+        get("/people/*/contacts", "application/json", (request, response) -> {
+            return getContacts();
         }, new JsonTransformer());
+    }
+
+    private static List<Person> getContacts() {
+        List<Person> people = new ArrayList<Person>();
+        people.add(new Person("Alex Sneddon", "alex"));
+        people.add(new Person("Will Crossland", "will"));
+        people.add(new Person("Krishnan Sambasivan", "krishnan"));
+        people.add(new Person("Roberto Nerici", "roberto"));
+        return people;
     }
 }
