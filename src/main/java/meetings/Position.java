ca149cf7fa584bf4a65e6629c3ea1ffad5e3c542
Alex Sneddon <snedda92@mc-s093801.hack.com>
2014-10-25 20:54:57 +0100

    updated position.java to contain position

--- src/main/java/meetings/Position.java
+++ src/main/java/meetings/Position.java
@@ -1,4 +1,12 @@
 package meetings;
 
 public class Position {
+    final public float latitude;
+    final public float longitude;
+
+    public Position (float latitude, float longitude)
+    {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
 }
