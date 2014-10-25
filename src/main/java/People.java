3f5cb148eb6c2d48c44d4c180ec854bc6062b089
roberton <roberto.nerici@bbc.co.uk>
2014-10-25 15:07:10 +0100

    init

---
+++ src/main/java/People.java
@@ -0,0 +1,9 @@
+import static spark.Spark.*;
+
+public class People {
+
+    public static void main(String[] args) {
+        setPort(Integer.parseInt(System.getenv("PORT")));
+        get("/people", (req, res) -> "[]");
+    }
+}
